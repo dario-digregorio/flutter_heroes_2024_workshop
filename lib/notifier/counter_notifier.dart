1c94c0de231ca19eec02d41291573d70de872362
Dario Digregorio <info@dario-digregorio.de>
2024-02-22 10:58:21 +0100

    changes

---
+++ lib/notifier/counter_notifier.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CountersNotifier with ChangeNotifier {
+  final List<int> _counters = [0];
+  int _selectedIndex = 0;
+
+  int get selectedIndex => _selectedIndex;
+  int get selectedCounter => _counters[_selectedIndex];
+  List<int> get countersCount => _counters;
+
+  void select(int index) {
+    _selectedIndex = index;
+    notifyListeners();
+  }
+
+  void increment() {
+    _counters[_selectedIndex]++;
+    notifyListeners();
+  }
+
+  void addCounter() {
+    _counters.add(0);
+    notifyListeners();
+  }
+}
